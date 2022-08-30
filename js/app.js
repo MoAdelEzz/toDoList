ae652d2c56c7345458757a31f3029f0fbf609291
Mohammed Adel Mohammed <mohammedadelmohammedezzeldin@gmail.com>
2022-08-30 15:41:01 +0200

    edit 3

--- js/app.js
+++ js/app.js
@@ -123,24 +123,65 @@ document.addEventListener('click', function(e){
 }
 );
 
-//============================================================================================
-document.addEventListener('keypress', (e)=>{
+//===========================================================================================
+
+let betweenZeroTwo = function (x = "")
+{
+    return ( x == "Numpad0" || x == "Numpad1" || x == "Numpad2") || ( x == "Digit0" || x == "Digit1" || x == "Digit2");
+}
+
+let isDigit = function(x = "")
+{
+    let A = ( x == "Numpad0" || x == "Numpad1" || x == "Numpad2" || x == "Numpad3" ||  x == "Numpad4" || x == "Numpad5" || x == "Numpad6" || x == "Numpad7" || x == "Numpad8" || x == "Numpad9")
+    let B = ( x == "Digit0" || x == "Digit1" || x == "Digit2" || x == "Digit3" ||  x == "Digit4" || x == "Digit5" || x == "Digit6" || x == "Digit7" || x == "Digit8" || x == "Digit9")
+    return A||B;
+}
+
+let isMinutesFirst = function(x = "")
+{
+    let A = ( x == "Numpad0" || x == "Numpad1" || x == "Numpad2" || x == "Numpad3" ||  x == "Numpad4" || x == "Numpad5");
+    let B = ( x == "Digit0" || x == "Digit1" || x == "Digit2" || x == "Digit3" ||  x == "Digit4" || x == "Digit5");
+    return A||B;
+}
+
+let toNum = function(key = "")
+{
+    switch (key)
+    {
+        case "Numpad0" : case "Digit0" : return "0";
+        case "Numpad1" : case "Digit1" : return "1";
+        case "Numpad2" : case "Digit2" : return "2";
+        case "Numpad3" : case "Digit3" : return "3";
+        case "Numpad4" : case "Digit4" : return "4";
+        case "Numpad5" : case "Digit5" : return "5";
+        case "Numpad6" : case "Digit6" : return "6";
+        case "Numpad7" : case "Digit7" : return "7";
+        case "Numpad8" : case "Digit8" : return "8";
+        case "Numpad9" : case "Digit9" : return "9";
+    }
+}
+
+
+//===========================================================================================
+document.addEventListener('keydown', (e)=>{
+
+    console.log(e.code);
 
     if (e.target.classList.contains('time'))
     {
         
         let x = e.target.value;
     
-        if (x.length == 0 && (e.key == "1" || e.key == "0" || e.key == "2") )
+        if (x.length == 0 && betweenZeroTwo(e.code))
         {
             return;
         }
 
         if (x.length == 1)
         {
-            if ((x[0] == "1" || x[0] == "0") && (e.key >= "0" && e.key <= "9"))
+            if ( (x[0] >= "0" && x[0] <= "1") && isDigit(e.code))
             {
-                x += e.key;
+                x += toNum(e.code);
                 x += ":";
                 e.target.value = x;
                 e.preventDefault();
@@ -148,19 +189,19 @@ document.addEventListener('keypress', (e)=>{
 
             if (x[0] == "2" && (e.key >= "0" && e.key <= "3"))
             {
-                x += e.key;
+                x += toNum(e.code);
                 x += ":";
                 e.target.value = x;
                 e.preventDefault();
             }
         }
 
-        if (x.length == 3 && (e.key >= "0" && e.key <= "5"))
+        if (x.length == 3 && isMinutesFirst(e.code))
         {
             return;
         }
 
-        if (x.length == 4 && (e.key >= "0" && e.key <= "9"))
+        if (x.length == 4 && isDigit(e.code))
         {
             e.target.value += e.key;
             e.target.placeholder = e.target.value;   
@@ -176,8 +217,8 @@ document.addEventListener('keypress', (e)=>{
     
         }
 
-        e.preventDefault();
 
+        e.preventDefault();
     }
 
     if (e.target.classList.contains('title'))
